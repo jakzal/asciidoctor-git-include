9de7f990df2446121871940ccce74f5511ced5d6
Jakub Zalas <jakub@zalas.pl>
2023-06-09 11:45:27 +0100

    Pass content to the helper method to make the test more readable

--- test/extension_test.rb
+++ test/extension_test.rb
@@ -25,7 +25,7 @@ include::git@test/fixtures/only-in-repo.adoc[revision=1c2568efed7378acf8040e4393
     end
 
     test 'it includes a file from the specified repository path' do
-      dir = given_file_committed_to_fresh_repo("chapter01.adoc")
+      dir = given_file_committed_to_fresh_repo("chapter01.adoc", "Other repo.")
 
       input = <<-EOS
 include::git@chapter01.adoc[repository=#{dir}]
@@ -57,7 +57,7 @@ include::git@test/fixtures/multiline.adoc[lines=2..3;5]
     at_exit {
       FileUtils.remove_entry(dir)
     }
-    cmd = %(git init -q #{dir} && git -C #{dir} config --local user.email test@example.com && git -C #{dir} config --local user.name Test && echo 'Other repo.' > #{dir}/#{file_name} && git -C #{dir} add #{dir} && git -C #{dir} commit -m 'Initialise the repo')
+    cmd = %(git init -q #{dir} && git -C #{dir} config --local user.email test@example.com && git -C #{dir} config --local user.name Test && echo '#{content}' > #{dir}/#{file_name} && git -C #{dir} add #{dir} && git -C #{dir} commit -m 'Initialise the repo')
     %x{#{cmd}}
     dir
   end
